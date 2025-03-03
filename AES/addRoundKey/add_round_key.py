01020f5c35d6d0d4296266c06257b939c16cfa63
Shadowghost94 <salifououzerou25@gmail.com>
2025-03-03 15:00:38 +0100

    last-one

--- AES/addRoundKey/add_round_key.py
+++ AES/addRoundKey/add_round_key.py
@@ -1,4 +1,6 @@
 from pwn import xor
+
+# matrix 4x4 du bloc de message
 state = [
     [206, 243, 61, 34],
     [171, 11, 93, 31],
@@ -6,6 +8,8 @@ state = [
     [150, 3, 194, 51],
 ]
 
+# la clé de round correspondant
+# e.g: key[1] => round 1; key[2] => round 2; key[3] => round 3; ...
 round_key = [
     [173, 129, 68, 82],
     [223, 100, 38, 109],
@@ -14,8 +18,8 @@ round_key = [
 ]
 
 
-def add_round_key(s, k):
-    result = xor(s,k)
+def add_round_key(state, round_key):
+    result = xor(state, round_key)
     return result
 
 
---
+++ RSA/modulus_inutilis/working_with_field.py
@@ -0,0 +1,6 @@
+from sympy import mod_inverse
+p = 991
+g = 209
+
+flag = mod_inverse(g, 991)
+print(flag)
